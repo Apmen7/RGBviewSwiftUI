81e4f55072ecadd7461f2ccd8749939b700f537c
Armen Babayan <apmen7@gmail.com>
2023-07-23 20:55:17 +0300

    Initial Commit

---
+++ RGBviewSwiftUI/RGBviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBviewSwiftUIApp.swift
+//  RGBviewSwiftUI
+//
+//  Created by Armen on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
